016ca5b201d939cbebb44abe9923c487bdde5cd7
Chloe <chloe84@msn.com>
2021-12-16 20:12:06 -0800

    tictactoechanges

--- tictactoe.js
+++ tictactoe.js
@@ -1,26 +1,9 @@
 // create a bunch of divs of boxes for the player to put x's and o's in.
 const gameBoard = document.querySelector('.game-board');
-function handledBoxClicked (e){
-    e.target.innerText = currentPlayer;
-    currentPlayer = currentPlayer === 'X' ? 'O': 'X';
-}
-for(let i = 0; i < 9; i++){
- 
-const box = document.createElement('div');
-
-box.setAttribute('class', 'box');
-box.setAttribute('data', i);
-box.addEventListener('click', handledBoxClicked);
-
-gameBoard.appendChild(box);
-
-}
-
 let playerTurn = document.querySelector('.player-turn');
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 let gameActive = true;
-playerTurn.innerHTML = currentPlayer + "'s turn";
 let winningConditions=[
   [0,1,2],
   [3,4,5],
@@ -31,14 +14,67 @@ let winningConditions=[
   [0,4,8],
   [2,4,6],
 ];
+playerTurn.innerHTML = currentPlayer + "'s turn";
 
 const clearButton = document.querySelector('.clear-button');
 clearButton.addEventListener('click', () => {
-    const boxes = document.querySelectorAll('.box');
-    for(let i = 0; i < boxes.length; i++){
+  const boxes = document.querySelectorAll('.box');
+  for(let i = 0; i < boxes.length; i++){
     let currentBox = boxes[i];
     currentBox.innerText = '';
-    }
+    currentPlayer = 'X';
+    gameActive = true;
+    playerTurn.innerHTML = currentPlayer + " 's turn";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+
+  }
 })
 
+function handledBoxClicked (e){
+  if(gameActive === false)return
+  if(e.target.innerText !== '')return 
+  e.target.innerText = currentPlayer;
+  gameState[e.target.getAttribute('data')] = currentPlayer
+  
+  checkWin()
+  if(gameActive === false){
+    return
+  }
+  //currentPlayer = currentPlayer === 'X' ? 'O': 'X';
+  playerSwitch()
+}
+function playerSwitch(){
+  if(currentPlayer === 'X'){
+    currentPlayer = 'O'
+  }else{
+    currentPlayer = 'X'
+  }
+  playerTurn.innerHTML = currentPlayer + "'s turn";
+}
+
+function checkWin(){
+  for(let i = 0; i < winningConditions.length; i++){
+    let condition = winningConditions[i]
+    //[2,4,6]
+    if(
+      gameState[condition[0]] === gameState[condition[1]] &&
+      gameState[condition[0]] === gameState[condition[2]] &&
+      gameState[condition[0]] !== "" 
+      ){
+      console.log('winner')
+      playerTurn.innerHTML = currentPlayer + " Won!";
+      gameActive = false
+    }
+  }  
+}
+
+for(let i = 0; i < 9; i++){
+  const box = document.createElement('div');
+
+  box.setAttribute('class', 'box');
+  box.setAttribute('data', i);
+  box.addEventListener('click', handledBoxClicked);
+
+  gameBoard.appendChild(box);
+}
 
